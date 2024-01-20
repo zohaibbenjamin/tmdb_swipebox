1479388fc700f619a5128aba7ab9c4d1b5316d8e
Zohaib Benjamin <zohaibbenjamin@gmail.com>
2024-01-20 16:09:47 +0500

    [Project Setup] Initial Commit Version 1.0

---
+++ tmdb_swipebox/tmdb_swipebox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tmdb_swipebox
+//
+//  Created by Zohaib Benjamin on 20/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
